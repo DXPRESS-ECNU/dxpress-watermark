1093584060b51ef66dffb2f00b122e47283743cc
qcmiao1998 <qcmiao@hotmail.com>
2018-12-24 21:39:37 +0800

    Add a publish zip tool.

---
+++ publish.ps1
@@ -0,0 +1,10 @@
+﻿# Just a zip tool now.
+$version = Read-Host 'Version'
+$pubfileList = Get-ChildItem  '.\Watermark\Properties\PublishProfiles\' -recurse -Include *.pubxml
+msbuild -t:restore
+foreach ($file in $pubfileList) {
+    $pubName = [System.IO.Path]::GetFileNameWithoutExtension($file);
+    # msbuild .\Watermark\Watermark.csproj /p:DeployOnBuild=true /p:PublishProfile=$pubName;
+	# Cannot run on msbuild 15;
+    Compress-Archive -Path .\Watermark\bin\publish\$pubName\* -DestinationPath $(".\Watermark\bin\publish\watermark_" + $version + "_" + $pubName + ".zip")
+}
