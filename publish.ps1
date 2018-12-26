4e8219971c8286928a954ac9683b5bfbd88d826e
qcmiao1998 <qcmiao@hotmail.com>
2018-12-26 16:48:23 +0800

    Update publish script.

--- publish.ps1
+++ publish.ps1
@@ -1,10 +1,11 @@
-﻿# Just a zip tool now.
+﻿# Build and publish.
 $version = Read-Host 'Version'
-$pubfileList = Get-ChildItem  '.\Watermark\Properties\PublishProfiles\' -recurse -Include *.pubxml
-# msbuild -t:restore
-foreach ($file in $pubfileList) {
-    $pubName = [System.IO.Path]::GetFileNameWithoutExtension($file);
-    # msbuild .\Watermark\Watermark.csproj /p:DeployOnBuild=true /p:PublishProfile=$pubName;
-	# Cannot run on msbuild 15;
-    Compress-Archive -Path .\Watermark\bin\publish\$pubName\* -DestinationPath $(".\Watermark\bin\publish\watermark_" + $version + "_" + $pubName + ".zip")
+$runtime = @("win-x64","linux-x64","osx-x64")
+dotnet restore
+foreach ($rt in $runtime) {
+    dotnet publish -f netcoreapp2.0 -c Release -r $rt -o ./bin/Publish/$rt;
+    Compress-Archive -Path .\Watermark\bin\Publish\$rt\* -DestinationPath $(".\Watermark\bin\publish\watermark_" + $version + "_" + $rt + ".zip")
 }
+# For pure net core
+dotnet publish -f netcoreapp2.0 -c Release -o ./bin/Publish/netcore20
+Compress-Archive -Path .\Watermark\bin\Publish\netcore20\* -DestinationPath $(".\Watermark\bin\publish\watermark_" + $version + "_netcore20.zip")
